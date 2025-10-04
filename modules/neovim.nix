864ce957dc77643e692e45b129eacb59392fb607
A. Thallemer <airan.thallemer@tum.de>
2025-10-05 00:18:21 +0200

    minor change

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,20 +1,19 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
+{ config, pkgs, lib, ... }:
+
+let
   nvimConfig = pkgs.fetchgit {
     url = "https://github.com/eigensum/nvim";
-    rev = "";
-    sha256 = "";
+    rev = "<commit-hash>";   # replace with actual commit
+    sha256 = "<sha256>";      # replace with actual sha256
   };
-in {
+in
+{
   options.programs.myNeovim.enable = lib.mkEnableOption "Enable eigensum's Neovim setup";
 
   config = lib.mkIf config.programs.myNeovim.enable {
     programs.neovim = {
       enable = true;
+
       extraPackages = with pkgs; [
         ripgrep
         lazygit
@@ -26,40 +25,38 @@ in {
         opam
 
         # LSPs
-        rustup # Rust
-        java-language-server # Java
-        rocmPackages.clang # C/C++
-        marksman # Markdown
-        python313Packages.python-lsp-server # Python
-        nil # Nix
-        ocamlPackages.ocaml-lsp # OCaml
-        zls # Zig
-        texlab # LaTeX
+        rustup
+        java-language-server
+        rocmPackages.clang
+        marksman
+        python313Packages.python-lsp-server
+        nil
+        ocamlPackages.ocaml-lsp
+        zls
+        texlab
         zathura 
-        lua-language-server # Lua
+        lua-language-server
 
         # Formatters
-        # rustfmt included in rustup
-        google-java-format # Java
-        # clang-format included in rocm.Packages.clang
-        mdformat # Markdown
-        black # Python
-        alejandra # Nix
-        ocamlformat_0_26_2 # OCaml
-        tex-fmt # Latex
-        stylua # Lua
+        google-java-format
+        mdformat
+        black
+        alejandra
+        ocamlformat_0_26_2
+        tex-fmt
+        stylua
       ];
+
+      extraConfig = ''
+        set spell
+        set spelllang=de,en
+        set spellsuggest=best,9
+      '';
     };
 
-    extraConfig = ''
-    set spell 
-    set spelllang=de,en
-    set spellsuggest=best,9
-    ''
-
-home.file = {
-  ".config/nvim".source = nvimConfig;
-};
-
+    home.file = {
+      ".config/nvim/init.lua".source = "${nvimConfig}/init.lua";
+    };
   };
 }
+
