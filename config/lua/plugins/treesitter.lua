b2355e6164a7e183f5769d892adff38de8db2e51
A. Thallemer <airan.thallemer@tum.de>
2025-10-05 19:16:20 +0200

    fixed nixos issues

--- config/lua/plugins/treesitter.lua
+++ config/lua/plugins/treesitter.lua
@@ -2,8 +2,13 @@
 -- configurations manager for neovim, especially useful for parsers
 --Github: https://github.com/nvim-treesitter/nvim-treesitter
 
+require("nvim-treesitter.install").prefer_git = true 
+require("nvim-treesitter.install").compilers = { "gcc" } 
+
 require("nvim-treesitter.configs").setup({
   ensure_installed = "maintained",
   highlight = { enable = true },
   indent = { enable = true },
 })
+
+require("nvim-treesitter.install").parser_install_dir = vim.fn.stdpath("data") .. "/site/treesitter_parsers"
