43f1b8a0e3c90349c872b46ff59a6239db150af2
A. Thallemer <airan.thallemer@tum.de>
2025-10-05 17:46:26 +0200

    removed lazy, purely declerative setup

--- config/init.lua
+++ config/init.lua
@@ -1,15 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
@@ -21,24 +9,22 @@ vim.opt.wrap = false
 
 vim.g.mapleader = " "
 
-require("lazy").setup({
-	{ require("plugins.treesitter") },
-	{ require("plugins.telescope") },
-	{ require("plugins.lualine") },
-	{ require("plugins.autopairs") },
-	{ require("plugins.catppuccin") },
-	{ require("plugins.alpha") },
-	{ require("plugins.tree") },
-	{ require("plugins.conform") },
-	{ require("plugins.lazygit") },
-	{ require("plugins.lspconfignvim") },
-	{ require("plugins.cmp") },
-	{ require("plugins.navbuddy") },
-	{ require("plugins.whichkey") },
-	{ require("plugins.gen") },
-	{ require("plugins.trouble") },
-	{ require("plugins.todo") },
-	{ require("plugins.urlopen") },
-	{ require("plugins.barbar") },
-	{ require("plugins.rendermarkdown") },
-})
+require("plugins.treesitter")
+require("plugins.telescope")
+require("plugins.lualine")
+require("plugins.autopairs")
+require("plugins.catppuccin")
+require("plugins.alpha")
+require("plugins.tree")
+require("plugins.conform")
+require("plugins.lazygit")
+require("plugins.lspconfignvim")
+require("plugins.cmp")
+require("plugins.navbuddy")
+require("plugins.whichkey")
+require("plugins.gen")
+require("plugins.trouble")
+require("plugins.todo")
+require("plugins.urlopen")
+require("plugins.barbar")
+require("plugins.rendermarkdown")
--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,45 +1,73 @@
-{ config, pkgs, lib, eigen-neovim, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  eigen-neovim,
+  ...
+}: {
   options.programs.myNeovim.enable = lib.mkEnableOption "Enable eigensum's Neovim setup";
 
   config = lib.mkIf config.programs.myNeovim.enable {
     programs.neovim = {
       enable = true;
 
-      extraPackages = with pkgs; [
-        ripgrep
-        lazygit
-        ollama
-        lua
-        zig
-        python313
-        ocaml
-        opam
-
-        # LSPs
-        rustup
-        java-language-server
-        marksman
-        python313Packages.python-lsp-server
-        nil
-        ocamlPackages.ocaml-lsp
-        zls
-        texlab
-        lua-language-server
-
-        # Formatters
-        google-java-format
-        mdformat
-        black
-        alejandra
-        ocamlformat_0_26_2
-        tex-fmt
-        stylua
-      ] ++ lib.optionals pkgs.stdenv.isLinux [
-        zathura
-        rocmPackages.clang
+      plugins = with pkgs.vimPlugins; [
+        nvim-treesitter
+        telescope-nvim
+        lualine-nvim
+        nvim-autopairs
+        catppuccin-nvim
+        alpha-nvim
+        nvim-tree-lua
+        conform-nvim
+        lazygit-nvim
+        nvim-lspconfig
+        nvim-cmp
+        navbuddy-nvim
+        which-key-nvim
+        gen-nvim
+        trouble-nvim
+        todo-comments-nvim
+        urlopen-nvim
+        barbar-nvim
+        markdown-nvim
       ];
+
+      extraPackages = with pkgs;
+        [
+          ripgrep
+          lazygit
+          ollama
+          lua
+          zig
+          python313
+          ocaml
+          opam
+
+          # LSPs
+          rustup
+          java-language-server
+          marksman
+          python313Packages.python-lsp-server
+          nil
+          ocamlPackages.ocaml-lsp
+          zls
+          texlab
+          lua-language-server
+
+          # Formatters
+          google-java-format
+          mdformat
+          black
+          alejandra
+          ocamlformat_0_26_2
+          tex-fmt
+          stylua
+        ]
+        ++ lib.optionals pkgs.stdenv.isLinux [
+          zathura
+          rocmPackages.clang
+        ];
 
       extraConfig = ''
         set spell
@@ -49,7 +77,7 @@
     };
 
     home.file = {
-    ".config/nvim".source = eigen-neovim + "/config";
+      ".config/nvim".source = eigen-neovim + "/config";
     };
   };
 }
