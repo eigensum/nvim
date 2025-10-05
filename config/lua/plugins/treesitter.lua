334cbe956156b20fa7ec0fcb9b80790318b97746
A. Thallemer <airan.thallemer@tum.de>
2025-10-05 19:33:52 +0200

    fix for treesitter

--- config/lua/plugins/treesitter.lua
+++ config/lua/plugins/treesitter.lua
@@ -2,12 +2,18 @@
 -- configurations manager for neovim, especially useful for parsers
 --Github: https://github.com/nvim-treesitter/nvim-treesitter
 
+local treesitter_install = require("nvim-treesitter.install")
+treesitter_install.parser_install_dir = vim.fn.stdpath("data") .. "/site/treesitter_parsers"
 
-require('nvim-treesitter.configs').setup {
-  parser_install_dir = vim.fn.stdpath("data") .. "/treesitter",
-  ensure_installed = "maintained",
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "lua", "vim", "vimdoc", "markdown", "bash", "python", "rust", "javascript", 
+    "typescript", "html", "css", "json", "yaml", "nix", "java", "ocaml", "zig",
+    "c", "cpp", "asm", "matlab", "latex"
+  },
+  
+  auto_install = true,
+  
   highlight = { enable = true },
   indent = { enable = true },
-}
-
-vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/treesitter")
+})
