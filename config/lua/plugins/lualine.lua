34eb14719f4ac96ec189fead3e703544ba67f512
A. Thallemer <airan.thallemer@tum.de>
2025-12-18 13:44:00 +0100

    added new language display function, qoute

--- config/lua/plugins/lualine.lua
+++ config/lua/plugins/lualine.lua
@@ -1,35 +1,42 @@
 -- lualine.nvim
 -- a status line for neovim
 -- Github: https://github.com/nvim-lualine/lualine.nvim
-
 local function spell_check()
-	local language = vim.opt.spelllang:get()[1]
-	local active = vim.opt.spell:get()
-	if not active then
+	if not vim.opt.spell:get() then
 		return "None"
 	end
-	if language == "en" then
-		return "English"
-	elseif language == "de" then
-		return "German"
-	else
-		return language
-	end
-end
-	require("lualine").setup({
-		options = {
-			theme = "catppuccin",
-			section_seperators = { left = " ", right = " " },
-			component_seperators = { left = " ", right = " " },
-			globalstatus = true,
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff" },
-			lualine_c = { "filename", spell_check },
-			lualine_x = { "encoding", "filetype" },
-			lualine_y = { "lsp_status" },
-			lualine_z = { "location" },
-		},
-	})
 
+	local langs = vim.opt.spelllang:get()
+
+	if not langs or #langs == 0 then
+		return "None"
+	end
+
+	local name_map = {
+		en = "English",
+		de = "German",
+	}
+
+	local display = {}
+	for _, lang in ipairs(langs) do
+		table.insert(display, name_map[lang] or lang)
+	end
+
+	return table.concat(display, ", ")
+end
+require("lualine").setup({
+	options = {
+		theme = "catppuccin",
+		section_seperators = { left = " ", right = " " },
+		component_seperators = { left = " ", right = " " },
+		globalstatus = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff" },
+		lualine_c = { "filename", spell_check },
+		lualine_x = { "encoding", "filetype" },
+		lualine_y = { "lsp_status" },
+		lualine_z = { "location" },
+	},
+})
